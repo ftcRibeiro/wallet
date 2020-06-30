31a93f5168b251876048f89fb26d88e96f3d3c8f
Felipe Ribeiro <felipe.ribeiro@ihm.com.br>
2020-06-29 21:28:02 -0300

    Create user basic controller

---
+++ src/main/java/com/wallet/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.wallet.controller;
+
+import com.wallet.entity.User;
+import com.wallet.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Response<UserDTO>> create(@Valid @RequestBody UserDTO dto, BindingResult result){
+
+        Response<UserDTO> response = new Response<UserDTO>();
+        
+        User user = userService.save(this.dtoToEntity(dto));
+        response.setData(this.entityToDto(user));
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    private User dtoToEntity(UserDTO dto){
+        User u = new User();
+        u.setEmail(dto.getEmail());
+        u.setName(dto.getName());
+        u.setPassword(dto.getPassword());
+        return u;
+    }
+
+    private UserDTO entityToDto(User u){
+        UserDTO dto = new UserDTO();
+        dto.setEmail(u.getEmail());
+        dto.setName(u.getName());
+        dto.setPassword(u.getPassword());
+        return u;
+    }
+
+}
